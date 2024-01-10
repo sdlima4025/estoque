dc4640cf057d97ee49e17794ae36e347ebedae00
sdlima4025 <sergiolimafg3@gmail.com>
2024-01-10 19:55:30 -0300

    criado css de da tela de login e estilizado.

--- views/login.php
+++ views/login.php
@@ -1,16 +1,37 @@
-<form method="POST">
-    Seu Número:<br/>
-    <input type="text" name="number"><br/><br/>
+<!DOCTYPE html>
+<html lang="en">
 
-    Sua Senha:<br/>
-    <input type="password" name="password"><br/><br/>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/login.css">
+</head>
 
-    <input type="submit" value="Entrar">
-</form>
+<body>
+    <div class="container">
+        <div class="loginarea">
+            <form method="POST">
+                Seu Número:<br />
+                <input type="text" name="number"><br /><br />
+    
+                Sua Senha:<br />
+                <input type="password" name="password"><br /><br />
+    
+                <input type="submit" value="Entrar">
+            </form>
+    
+            <?php
+            if (!empty($msg)) : ?>
+    
+                <h2><?php echo $msg; ?></h2>
+    
+            <?php endif; ?>
+    
+        </div>
 
-<?php
-if(!empty($msg)): ?>
+    </div>
 
-<h2><?php echo $msg; ?></h2>
+</body>
 
-<?php endif; ?>
+</html>
